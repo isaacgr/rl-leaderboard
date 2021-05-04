6cf8df0f940bf7ec05538c9a043566b4fdae0b75
isaacgr <isaacgrowell@gmail.com>
2021-05-03 20:35:13 -0400

    Remove client folder

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Players from "./components/Players";
 import "./App.css";
 
 const client = new ApolloClient({
-  uri: "/graphql",
+  uri: "http://localhost:5000/graphql",
   cache: new InMemoryCache()
 });
 
